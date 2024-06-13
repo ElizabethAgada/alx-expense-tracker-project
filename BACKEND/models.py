10ca50ac99c4aeddbab2941e0f241a81415ab8a8
ElizabethAgada <140746363+ElizabethAgada@users.noreply.github.com>
2024-06-13 19:19:48 +0100

    Create models.py

---
+++ BACKEND/models.py
@@ -0,0 +1,10 @@
+from flask_login import UserMixin
+
+class User(UserMixin):
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
+
+# In-memory user store
+users = {}
